49768b53e1f8eb213a24157482cfb2322e54157a
Anton Södergren <anton.sodergren@gmail.com>
2017-02-01 23:05:02 +0100

    Integrated camera with character controller

--- Assets/Scripts/Player/CharacterInput.cs
+++ Assets/Scripts/Player/CharacterInput.cs
@@ -7,8 +7,10 @@ namespace UnityStandardAssets.Characters.ThirdPerson
     [RequireComponent(typeof(CustomCharacterController))]
     public class CharacterInput : MonoBehaviour
     {
+        [Tooltip("The object that controls the character's direction")]
+        public Transform Direction;
+
         private CustomCharacterController m_Character; // A reference to the ThirdPersonCharacter on the object
-        private Transform m_Cam;                  // A reference to the main camera in the scenes transform
         private Vector3 m_CamForward;             // The current forward direction of the camera
         private Vector3 m_Move;
         private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
@@ -16,18 +18,6 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
         private void Start()
         {
-            // get the transform of the main camera
-            if (Camera.main != null)
-            {
-                m_Cam = Camera.main.transform;
-            }
-            else
-            {
-                Debug.LogWarning(
-                    "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
-                // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
-            }
-
             // get the third person character ( this should never be null due to require component )
             m_Character = GetComponent<CustomCharacterController>();
         }
@@ -50,17 +40,11 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             float v = CrossPlatformInputManager.GetAxis("Vertical");
 
             // calculate move direction to pass to character
-            if (m_Cam != null)
-            {
+            
                 // calculate camera relative direction to move:
-                m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-                m_Move = v * m_CamForward + h * m_Cam.right;
-            }
-            else
-            {
-                // we use world-relative directions in the case of no main camera
-                m_Move = v * Vector3.forward + h * Vector3.right;
-            }
+                m_CamForward = Vector3.Scale(Direction.forward, new Vector3(1, 0, 1)).normalized;
+                m_Move = v * m_CamForward + h * Direction.right;
+     
 
             // pass all parameters to the character control script
             m_Character.Move(m_Move, m_Jump);
