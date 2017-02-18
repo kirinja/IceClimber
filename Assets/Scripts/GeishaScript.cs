32f464c8a8cd6e09a3523e804b716e72e7c91ba0
Miggli95 <miggli@hotmail.se>
2017-02-18 18:21:43 +0100

    Geisha hjälper en att komma upp för berget

---
+++ Assets/Scripts/GeishaScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeishaScript : MonoBehaviour
+{
+    public float force;
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+           other.GetComponent<Rigidbody>().AddForce(Vector3.up*force);
+        }
+    }
+	
+}
